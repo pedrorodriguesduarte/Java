0fd45a5b1dab238fe9ccb7c5361aaae9e98d4940
Pedro Rodrigues Duarte <artedudurty@gmail.com>
2025-02-28 08:20:42 -0300

    Add files via upload

---
+++ Projeto pessoa/Main.java
@@ -0,0 +1,11 @@
+// Classe principal
+public class Main {
+    public static void main(String[] args) {
+        // Criando um objeto da classe Pessoa
+        Pessoa pessoa1 = new Pessoa("Hugo", 52);
+
+        // Chamando o método da classe Pessoa
+        pessoa1.exibirInformacoes();
+    }
+}
+
---
+++ Projeto pessoa/Pessoa.java
@@ -0,0 +1,17 @@
+// Classe Pessoa
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    // Construtor
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    // Método para exibir informações
+    public void exibirInformacoes() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade + " anos");
+    }
+}
